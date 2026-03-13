00eba7bb93601ef6b381225d0b439df21aa5c347
Andy Le <andylequ@gmail.com>
2026-03-13 00:26:04 -0500

    initialized websocketserver and clients as Map

--- server/server.js
+++ server/server.js
@@ -4,15 +4,19 @@ const fs = require("fs");
 const path = require("path");
 
 // Serve the client HTML file
-const httpServer = http.createServer((req, res)) => {
-  const filePath = path.join(__dirname, 'client.html');
+const httpServer = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "client.html");
   fs.readFile(filePath, (err, data) => {
-    if(err) {
+    if (err) {
       res.writeHead(404);
       res.end("Not found");
       return;
     }
     res.writeHead(200);
     res.end(data);
-  })
-}
+  });
+});
+
+const wss = new WebSocketServer({ server: httpServer });
+
+const clients = new Map();
