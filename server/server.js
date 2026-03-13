c728ca06415e7b3d46b41bf61a1e28a739b78ea7
Andy Le <andylequ@gmail.com>
2026-03-13 00:12:44 -0500

    started writing the server.js logic out

--- server/server.js
+++ server/server.js
@@ -3,4 +3,16 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-
+// Serve the client HTML file
+const httpServer = http.createServer((req, res)) => {
+  const filePath = path.join(__dirname, 'client.html');
+  fs.readFile(filePath, (err, data) => {
+    if(err) {
+      res.writeHead(404);
+      res.end("Not found");
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  })
+}
